a040df352026755f9d191e417c90fdc72b9944a6
AMarinaG <alberto.marina.gonzalo@gmail.com>
2018-04-19 00:21:24 +0200

    basic provide viewmodel

---
+++ app/src/main/java/com/amarinag/amgmoviedb/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.amarinag.amgmoviedb
+
+import android.arch.lifecycle.ViewModel
+import android.util.Log
+
+/**
+ *      AMGMovieDB.
+ *
+ *  @author -   AMarinaG
+ *  @since  -   19/4/18
+ */
+class MainViewModel : ViewModel() {
+    init {
+        Log.d("MainViewModel", "Se ha iniciado MainViewModel")
+    }
+}
