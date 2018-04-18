bec2ca4cb9d6d22a401809791ecac8e3a13abc34
AMarinaG <alberto.marina.gonzalo@gmail.com>
2018-04-19 00:14:58 +0200

    basic MainActivity binding

--- app/src/main/java/com/amarinag/amgmoviedb/MainActivity.kt
+++ app/src/main/java/com/amarinag/amgmoviedb/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.amarinag.amgmoviedb
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.amarinag.amgmoviedb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
