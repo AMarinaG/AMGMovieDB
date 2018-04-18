f7274b03f62029d12a8853056590455765a3a269
AMarinaG <alberto.marina.gonzalo@gmail.com>
2018-04-19 01:37:13 +0200

    basic retrofit

--- app/src/main/java/com/amarinag/amgmoviedb/di/module/AppModule.kt
+++ app/src/main/java/com/amarinag/amgmoviedb/di/module/AppModule.kt
@@ -1,7 +1,18 @@
 package com.amarinag.amgmoviedb.di.module
 
+import com.amarinag.amgmoviedb.BuildConfig
 import com.amarinag.amgmoviedb.di.ViewModelModule
+import com.amarinag.amgmoviedb.network.MovieDbService
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 /**
  *      AMGMovieDB.
@@ -11,4 +22,30 @@ import dagger.Module
  */
 @Module(includes = [ViewModelModule::class])
 class AppModule {
+    @Provides
+    @Singleton
+    internal fun provideGson(): Gson {
+        return GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .create()
+    }
+
+    @Provides
+    @Singleton
+    internal fun provideOkHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+                .readTimeout(120, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
+        builder.addNetworkInterceptor(StethoInterceptor())
+        return builder.build()
+    }
+
+    @Provides
+    @Singleton
+    internal fun providesMovieDbService(okHttpClient: OkHttpClient, gson: Gson) = Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(MovieDbService::class.java)
 }
