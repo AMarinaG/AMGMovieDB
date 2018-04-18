22bdf274d9c6f8436e71f0b8233d0a71db900221
AMarinaG <alberto.marina.gonzalo@gmail.com>
2018-04-19 00:25:32 +0200

    move activity to new package

---
+++ app/src/main/java/com/amarinag/amgmoviedb/main/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.amarinag.amgmoviedb.main
+
+import android.arch.lifecycle.ViewModel
+import timber.log.Timber
+
+/**
+ *      AMGMovieDB.
+ *
+ *  @author -   AMarinaG
+ *  @since  -   19/4/18
+ */
+class MainViewModel : ViewModel() {
+    init {
+        Timber.d("Se ha iniciado MainViewModel")
+    }
+}
