21c3f27aa262e91e3d6ae7fb865128e839b4c9dd
AMarinaG <alberto.marina.gonzalo@gmail.com>
2018-04-19 22:33:41 +0200

    create basic adapter

---
+++ app/src/main/java/com/amarinag/amgmoviedb/ui/main/MovieAdapter.kt
@@ -0,0 +1,39 @@
+package com.amarinag.amgmoviedb.ui.main
+
+import android.databinding.DataBindingUtil
+import android.support.v7.recyclerview.extensions.ListAdapter
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.amarinag.amgmoviedb.R
+import com.amarinag.amgmoviedb.databinding.ItemMovieBinding
+import com.amarinag.amgmoviedb.model.Movie
+
+/**
+ *      AMGMovieDB.
+ *
+ *  @author -   AMarinaG
+ *  @since  -   19/4/18
+ */
+class MovieAdapter(private val clickListener: (Movie) -> Unit) : ListAdapter<Movie, MovieAdapter.MovieViewHolder>(MovieDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val binding: ItemMovieBinding =
+                DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.item_movie, parent, false)
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(Movie(position, "Name $position"), clickListener)
+
+    }
+
+    override fun getItemCount() = 15
+
+    inner class MovieViewHolder(val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(movie: Movie, clickListener: (Movie) -> Unit) {
+            binding.movie = movie
+            binding.root.setOnClickListener { clickListener(movie) }
+        }
+    }
+
+}
