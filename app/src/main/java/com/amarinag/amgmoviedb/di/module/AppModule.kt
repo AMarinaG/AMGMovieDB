c0008caa1c821d5ebd0fafb544e60ccfc6e7fd69
AMarinaG <alberto.marina.gonzalo@gmail.com>
2018-04-20 08:17:49 +0200

    add rxcalladapter to retrofit

--- app/src/main/java/com/amarinag/amgmoviedb/di/module/AppModule.kt
+++ app/src/main/java/com/amarinag/amgmoviedb/di/module/AppModule.kt
@@ -10,6 +10,7 @@ import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
@@ -46,6 +47,7 @@ class AppModule {
             .baseUrl(BuildConfig.BASE_URL)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
             .create(MovieDbService::class.java)
 }
