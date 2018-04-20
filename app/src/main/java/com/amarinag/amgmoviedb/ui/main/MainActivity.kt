3a144ae170b2b251db84c32ecbe2bc70a205d4c6
AMarinaG <alberto.marina.gonzalo@gmail.com>
2018-04-21 01:09:43 +0200

    update item list

--- app/src/main/java/com/amarinag/amgmoviedb/ui/main/MainActivity.kt
+++ app/src/main/java/com/amarinag/amgmoviedb/ui/main/MainActivity.kt
@@ -3,7 +3,7 @@ package com.amarinag.amgmoviedb.ui.main
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import com.amarinag.amgmoviedb.R
 import com.amarinag.amgmoviedb.databinding.ActivityMainBinding
 import com.amarinag.amgmoviedb.ui.base.BaseActivity
@@ -20,7 +20,7 @@ class MainActivity : BaseActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         mainViewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
         val adapter = MovieAdapter({ movie -> Timber.d("Has pulsado la peli: %s", movie) })
-        binding.rvMovies.layoutManager = GridLayoutManager(this, 2)
+        binding.rvMovies.layoutManager = LinearLayoutManager(this)
         binding.rvMovies.adapter = adapter
         mainViewModel.getPopular(1)
                 .subscribeOn(Schedulers.io())
