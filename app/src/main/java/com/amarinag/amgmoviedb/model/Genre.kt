5c6f603cc90aa0d2745f1942fe544b947f6cb424
AMarinaG <alberto.marina.gonzalo@gmail.com>
2018-04-22 17:44:53 +0200

    update model

---
+++ app/src/main/java/com/amarinag/amgmoviedb/model/Genre.kt
@@ -0,0 +1,20 @@
+package com.amarinag.amgmoviedb.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+
+/**
+ *      AMGMovieDB.
+ *
+ *  @author -   AMarinaG
+ *  @since  -   22/4/18
+ */
+data class Genre(
+        @SerializedName("id")
+        @Expose
+        val id: Int = 0,
+        @SerializedName("name")
+        @Expose
+        val name: String? = null
+)
