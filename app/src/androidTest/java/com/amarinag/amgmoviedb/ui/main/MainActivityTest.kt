6d875e28df563c8e8df5ad11d623b3f4af155485
AMarinaG <alberto.marina.gonzalo@gmail.com>
2018-04-23 05:37:30 +0200

    added test for details view and migrate all test to kotlin

---
+++ app/src/androidTest/java/com/amarinag/amgmoviedb/ui/main/MainActivityTest.kt
@@ -0,0 +1,96 @@
+package com.amarinag.amgmoviedb.ui.main
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.Espresso.pressBack
+import android.support.test.espresso.IdlingRegistry
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import android.support.test.espresso.matcher.ViewMatchers.withClassName
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.support.v7.widget.RecyclerView
+import android.test.suitebuilder.annotation.LargeTest
+import android.view.View
+import android.view.ViewGroup
+import com.amarinag.amgmoviedb.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return (parent is ViewGroup
+                        && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position))
+            }
+        }
+    }
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        IdlingRegistry.getInstance().register(mActivityTestRule.activity.getIdlingResource())
+    }
+
+    @After
+    @Throws(Exception::class)
+    fun tearDown() {
+        IdlingRegistry.getInstance()
+                .unregister(mActivityTestRule.activity.getIdlingResource())
+    }
+
+    @Test
+    fun mainActivityTest() {
+        try {
+            Thread.sleep(1000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        val recyclerView = onView(
+                allOf(
+                        withId(R.id.rv_movies),
+                        childAtPosition(
+                                withClassName(
+                                        `is`(
+                                                "android.support.design.widget.CoordinatorLayout")),
+                                0)))
+        recyclerView.perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(1000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        pressBack()
+    }
+}
